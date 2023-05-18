d8810b20528570977cfacf5b8ddf0da99755b0bf
Juanesduque1 <juanesduquemorales@gmail.com>
2023-05-18 15:40:38 -0500

    [Fix]: Workflow fixed

--- module3_task5/setup.sh
+++ module3_task5/setup.sh
@@ -1,5 +1,16 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+#!/bin/bash
+apt update && apt upgrade -y
+apt install -y wget make dpkg
+
+# Hugo installation
+wget -O hugo_binary.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
+dpkg -i hugo_binary.deb
+rm hugo_binary.deb
+
+# golangci-lint installation
+wget -q -O golangci-lint.deb https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.deb
+dpkg -i golangci-lint.deb
+rm golangci-lint.deb
+
+# markdown installation
+npm install -g markdownlint-cli markdown-link-check
